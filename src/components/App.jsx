598b831ca79df13fa5fbf77b5a6205e6f52be3f8
Андрей Скляр <andreas.skliar@gmail.com>
2023-10-16 21:48:36 +0300

    пофіксив дрібні баги

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,21 @@
-import user from './Profile/user.json';
-import data from './Statistics/data.json';
+import user from 'data/user.json';
+import data from 'data/data.json';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 
 export const App = () => {
   return (
     <div>
-      <div>
-        <Profile
-          username={user.username}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
-        />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
 
-        <Statistics title="Upload stats" stats={data} />
-        {/* <Statistics stats={data} /> */}
-      </div>
+       <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics stats={data} /> */}
     </div>
   );
 };
