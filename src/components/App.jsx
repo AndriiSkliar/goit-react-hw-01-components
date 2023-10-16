fd0efba1429a71cdc4e8cad997e84d68f12255b5
Андрей Скляр <andreas.skliar@gmail.com>
2023-10-16 21:35:24 +0300

    зробив jsx 2-х задач

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,23 @@
+import user from './Profile/user.json';
+import data from './Statistics/data.json';
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+
 export const App = () => {
   return (
     <div>
+      <div>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
 
+        <Statistics title="Upload stats" stats={data} />
+        {/* <Statistics stats={data} /> */}
+      </div>
     </div>
   );
 };
